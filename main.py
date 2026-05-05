25fc9411b3a77ba6fb17758701f59bce3fc5a5e9
Wisdom-Like-Silence <202318930+Wisdom-Like-Silence@users.noreply.github.com>
2026-05-05 14:24:09 +0100

    draws player to the screen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 
 def main():
@@ -13,6 +13,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
 
@@ -20,8 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
 
