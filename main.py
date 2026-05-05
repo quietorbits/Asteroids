8448c27052ed2288b23bffdf5162aeaeef4fe2d7
Wisdom-Like-Silence <202318930+Wisdom-Like-Silence@users.noreply.github.com>
2026-05-05 13:28:13 +0100

    added circleshape.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,8 +22,8 @@ def main():
         
         
         screen.fill("black")
-        pygame.display.flip() 
-
+        pygame.display.flip()
+        dt = (clock.tick(60) / 1000)
 
 
 if __name__ == "__main__": main()
