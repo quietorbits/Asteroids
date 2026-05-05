6ab3ea250ebac2be2101cd682074e3d53caf4b9e
Wisdom-Like-Silence <202318930+Wisdom-Like-Silence@users.noreply.github.com>
2026-05-05 15:40:03 +0100

    movement, updateing and drawing by groups

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+
+
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -24,7 +29,9 @@ def main():
         
 
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
 
