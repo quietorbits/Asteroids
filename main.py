87aacf221b8ee58fed5cd5c663c3741b6dea1ac7
Wisdom-Like-Silence <202318930+Wisdom-Like-Silence@users.noreply.github.com>
2026-05-05 17:59:04 +0100

    Collision detection added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame
+import pygame, sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
@@ -38,6 +39,11 @@ def main():
         for item in drawable:
             item.draw(screen)
         updatable.update(dt)
+        for object in asteroids:
+            if player.collides_with(object):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
 
