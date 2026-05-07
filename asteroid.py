d22da9e2dbad48d1f518959c184380b037922110
Wisdom-Like-Silence <202318930+Wisdom-Like-Silence@users.noreply.github.com>
2026-05-07 20:49:03 +0100

    added asteroid destruction and spliting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,4 +14,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+  
+        vector_1 = self.velocity.rotate(random.uniform(20, 50))
+        vector_2 = self.velocity.rotate(random.uniform(-20, -50))
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = vector_1 * 1.2
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2.velocity = vector_2 * 1.2
+
 
